d805a76aa26163a0460f73882af755912eccf81c
Amrit066 <staining607@gmail.com>
2022-03-20 21:32:42 +0530

    Q1. Largest sum of subarray

---
+++ q1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int largetSumSubarray(int arr[],int n){
+    int largestSum=0;
+
+    for(int i=0;i<n;i++){
+        for(int j=i;i<n;j++){
+            int temp=0;
+            for(int k=i;k<=j;k++){
+                temp+=arr[k];
+                if(temp>largestSum){
+                    largestSum=temp;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    }
+
+    return largestSum;
+}
+
+int main(void){
+    int arr[10];
+    int n;
+
+    cout<<"Enter no of elements in array(<10): ";
+    cin>>n;
+
+    cout<<"Enter array elements: "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    
+    cout<<"Largest sum of all possible subarray is "<<largetSumSubarray(arr,n);
+
+    return 0;
+}
