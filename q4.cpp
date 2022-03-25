3b7c946e8aed02a059448209ae927f42e4f44151
Amrit066 <staining607@gmail.com>
2022-03-25 20:23:45 +0530

    Q4. Lower bound of a number in an array

---
+++ q4.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int lowerBound(vector<int> A, int Val){
+    int lowerBound=A[0];
+
+    for(int i=0;i<A .size();i++){
+        if(A[i]>lowerBound && A[i]<=Val){
+            lowerBound=A[i];
+        }
+    }
+
+    return lowerBound;
+}
+
+int main(void){
+    vector<int> arr;
+    int n,k;
+    
+    cout<<"Enter number elements of array"<<endl;
+    cin>>n;
+    cout<<"Enter each element of array"<<endl;
+    for(int i=0;i<n;i++){
+        int j=0;
+        cin>>j;
+        arr.push_back(j);
+    }
+    cout<<"Enter value "<<endl;
+    cin>>k;
+    
+    cout<<"Lower bound for "<<k<<" is " <<lowerBound(arr,k);
+
+    return 0;
+}
